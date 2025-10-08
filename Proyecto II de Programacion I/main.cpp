d79c715ee693a2964dc11a251f63f8a0a13ca65c
JP <josepablosanchezqwq@gmail.com>
2025-10-07 23:16:13 -0600

    Inicio

---
+++ Proyecto II de Programacion I/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	/*
+	Tabla de evaluación y calificación del proyecto :
+
+	Para cada rubro especificado a continuación se verificará el cumplimiento de todo lo
+		especificado al respecto en este documento.
+
+	Es necesario que se haga un correcto, adecuado y eficiente diseño e implementación
+		de clases y relaciones entre clases, de acuerdo con el POO.
+
+	Como se mencionó anteriormente, el sistema debe permitir al usuario ingresar todos
+		los datos requeridos para probar las funcionalidades.
+
+	Aunque cada rubro tiene una calificación individual, si la ausencia o mal
+		funcionamiento de una funcionalidad impide probar otra, se perderán los puntos de
+		los rubros asociados a ambas funcionalidades.
+
+	Datos Generales.
+
+	Ingreso, eliminación y visualización de :
+Sucursales(2 pts).
+Clientes por sucursal(2 pts).
+Colaborares por sucursal(2 pts).
+
+	Planteles, Parqueos y Vehículos :
+Creación de planteles de sucursal.Al crear un plantel de una sucursal
+		se debe ingresar la cantidad de espacios de estacionamiento disponibles
+		en el plantel, organizados por filas y columnas.Al crear un plantel se
+		debe mostrar gráficamente la distribución de los espacios de
+		estacionamiento disponibles en el plantel. (5 pts).
+
+Ingreso, eliminación y visualización de vehículos de una sucursal.
+		Recuerde que, al ingresar un vehículo, se debe vincular a un plantel y
+		espacio de estacionamiento específicos.Además, para estacionar un
+		vehículo, el sistema debe generar una recomendación de los espacios de
+		estacionamiento más adecuados. (9 pts).
+
+Reubicación de un vehículo en un plantel y un espacio de estacionamiento
+		dentro de la misma sucursal. (3 pts).
+
+Visualización de la distribución de espacios de estacionamiento de un
+		plantel, mostrando el estado de cada uno de los espacios de
+		estacionamiento(disponibles y ocupados).Debe permitir al usuario
+		ingresar un número de espacio de estacionamiento especifico, y mostrar
+		la placa del vehículo estacionado en dicho espacio. (5 pts).
+
+Cambio de estado de un vehículo(de acuerdo con la tabla de estados
+			presentada en el enunciado) y registro del cambio en la bitácora.(4 pts).
+
+(OPCIONAL) Traslado de vehículos de una sucursal a otra.Debe permitir
+		reubicar n vehículos de la sucursal A a la Sucursal B.Solo se pueden
+		reubicar los vehículos que no se encuentren alquilador.Al trasladar los
+		vehículos se deben ubicar en espacios de estacionamiento específicos de
+		la sucursal B, de acuerdo con las recomendaciones de espacios generadas
+		por el sistema. (7 pts).
+
+		Solicitudes y Contratos:
+Creación y visualización de solicitudes(de cada solicitud se debe
+			mostrar todo su detalle : sucursal, código, id cliente, placa del
+			vehículo, días de alquiler, fecha de inicio, fecha de entrega, precio
+			diario y total, estado, colaborador, etc.). (6 pts).
+
+Aprobación o rechazo de una solicitud de alquiler. (1 pto).
+Recepción de vehículo en alquiler y finalización del contrato.(3pts).
+	
+		Reportes de Cliente:
+Historial del cliente, reporte de todas las solicitudes y contratos de
+		alquiler realizados por un cliente especifico, detallando el código del
+		contrato / solicitud y la placa del vehículo. (9 pts).
+
+Reporte de clientes por cantidad de contratos : Ordena en la lista los
+		clientes por cantidad de alquileres realizados, iniciando por el cliente
+		que más alquileres haya hecho.Debe incluir el id y el nombre del
+		cliente, así como el código de los contratos de alquiler realizados. (8 pts).
+	
+		Reportes de Planteles y Vehículos:
+Estados de un vehículo especifico, se debe mostrar la bitácora con todos
+		los cambios de estado que ha tenido un vehículo especifico, incluyendo:
+	estado anterior, nuevo estado, fecha del cambio y colaborador que realizó
+		el cambio de estado. (6 pts).
+
+Reporte de contratos para vehículo especifico.Muestra todos los
+		contratos de alquiler para un vehículo especifico, detallando : placa del
+		vehículo, id de contrato, id del cliente, días de alquiler, fecha de
+		inicio, fecha de devolución, estado. (10 pts).
+
+Reporte de porcentaje de ocupación de los planteles.Se debe mostrar,
+		por sucursal, el porcentaje de ocupación actual de todos sus planteles.
+		(7 pts).
+
+		Reportes de alquileres:
+Visualización de todos los contratos por sucursal, ordenados del más
+		reciente al más antiguo, incluir: código, estado, fecha de inicio,
+		fecha de devolución, id del cliente, placa del vehículo, precio diario
+		y total, colaborador. (8 pts).
+
+Reporte alquileres por colaborador.Reporte de alquileres realizados por
+		un colaborador especifico, incluir : id y nombre del colaborador, código
+		del contrato, placa del vehículo y id del cliente. (10 pts).
+
+		Total 100
+
+		*/
+
+
+	return 0;
+}

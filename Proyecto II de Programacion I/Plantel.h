3fcbba2ca22f36fb311787f28fb10a936156c744
Jose Sánchez <josepablosanchezqwq@gmail.com>
2025-10-21 11:20:22 -0600

    Clase

--- Proyecto II de Programacion I/Plantel.h
+++ Proyecto II de Programacion I/Plantel.h
@@ -2,14 +2,14 @@
 class Plantel
 	/*
 	Cada una de las sucursales tiene n de planteles
-, cada uno Èstos dividido en varios espacios de estacionamiento. Cada plantel se identifica con una letra, adem·s registra una capacidad m·xima de vehÌculos que puede alojar y un tipo, el cual puede ser uno de los siguientes:
+, cada uno √©stos dividido en varios espacios de estacionamiento. Cada plantel se identifica con una letra, adem√°s registra una capacidad m√°xima de veh√≠culos que puede alojar y un tipo, el cual puede ser uno de los siguientes:
 - (1) Techado y asfaltado
 - (2) Techado y lastreado
 - (3) Asfaltado
 - (4) Lastreado
-Cada Plantel tiene m espacios de Estacionamiento. Cada espacio de estacionamientose identifica con un cÛdigo ˙nico, el cual est· compuesto por la letra del plantel y un n˙mero, por ejemplo: A01, A02, A03, B01, B02, B03. Cada espacio de estacionamiento, adem·s, tiene un estado (disponible u ocupado). Ejemplo:
+Cada Plantel tiene m espacios de Estacionamiento. Cada espacio de estacionamientose identifica con un c√≥digo √∫nico, el cual est√° compuesto por la letra del plantel y un n√∫mero, por ejemplo: A01, A02, A03, B01, B02, B03. Cada espacio de estacionamiento, adem√°s, tiene un estado (disponible u ocupado). Ejemplo:
 La sucursal de Heredia tiene dos planteles: el A y el B:
-1 Plantel: Es la instalaciÛn fÌsica destinada al resguardo, operaciÛn y mantenimiento de los vehÌculos de la empresa. En el plantel se realizan actividades como el estacionamiento de las unidades cuando no est·n en servicio, la limpieza, entre otros. A01
+1 Plantel: Es la instalaci√≥n f√≠sica destinada al resguardo, operaci√≥n y mantenimiento de los veh√≠culos de la empresa. En el plantel se realizan actividades como el estacionamiento de las unidades cuando no est√°n en servicio, la limpieza, entre otros. A01
 A02
 A03
 A04
@@ -17,10 +17,19 @@ A05
 A06
 B01 B02 B03 B04 B05 B06 B07 B08 B09 B10 B11 B12 B13 B14 B15 B16 B17 B18 B19 B20 B21 B22 B23 B24 B25 B26 B27 B28 B29 B30  Plantel A
 Tipo: 1
-Capacidad o estacionamientos: para 6 vehÌculos
+Capacidad o estacionamientos: para 6 veh√≠culos
 Plantel B Tipo: 2
-Capacidad o estacionamientos: para 30 vehÌculos
+Capacidad o estacionamientos: para 30 veh√≠culos
 	*/
 {
+private:
+string tipo;
+int capMax;
+EspacioEstacionamiento* estacionamiento;
+public:
+Plantel();
+Plantel();
+~Plantel();
+
 };
 

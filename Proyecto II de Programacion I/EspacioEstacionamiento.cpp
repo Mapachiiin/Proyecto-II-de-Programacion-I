f4c227cc1aac1f5e026d8b42cd095daf2835a502
JP <josepablosanchezqwq@gmail.com>
2025-10-21 22:34:08 -0600

    Espacios

--- Proyecto II de Programacion I/EspacioEstacionamiento.cpp
+++ Proyecto II de Programacion I/EspacioEstacionamiento.cpp
@@ -1,1 +1,48 @@
 #include "EspacioEstacionamiento.h"
+
+EspacioEstacionamiento::EspacioEstacionamiento(int capMax) {
+	if (esPrimo(capMax)) {
+		nF = 1;
+		nC = capMax;
+	}
+	else if (capMax % 2 == 0) {
+		nF = 2;
+		nC = capMax / 2;
+	}
+	else if (capMax % 7 == 0) {
+		nF = 7;
+		nC = capMax / 7;
+	}
+	else {
+		nF = 1;
+		nC = capMax;
+	}
+	espacios = new Vehiculo * *[nF];
+	for (int i = 0;i < nF;i++) {
+		espacios[i] = new Vehiculo * [nC];
+		for (int j = 0; j < nC;j++) {
+			espacios[i][j] = nullptr;
+		}
+	}
+}
+
+EspacioEstacionamiento::~EspacioEstacionamiento(){
+	for (int i = 0;i < nF;i++) {
+		delete[] espacios[i];
+}
+	delete[] espacios;
+}
+string EspacioEstacionamiento::espacioRecomendado(){
+	int temp=0;
+	int aux=0;
+	string espaReco = "";
+	for (int i = 0;i < nF;i++) {
+		for (int j = 0;j < nC;j++) {
+
+		}
+	}
+
+}
+bool EspacioEstacionamiento::esPrimo(int num) {
+
+}
--- Proyecto II de Programacion I/EspacioEstacionamiento.h
+++ Proyecto II de Programacion I/EspacioEstacionamiento.h
@@ -1,34 +1,28 @@
 #pragma once
+#include "Vehiculo.h"
+#include <iostream>
+using namespace std;
+
 class EspacioEstacionamiento
-
 	/*
-	
-	Cuando un vehículo es alquilado deja disponible su espacio en el estacionamiento y
-cuando es devuelto, el sistema debe recomendar un lugar específico para estacionar
-el vehículo que ingresa. El sistema debe recomendar primeramente el plantel de la
-sucursal con más espacios de estacionamiento disponibles. Una vez elegido el plantel,
-el sistema debe recomendar los espacios de estacionamiento con menos vehículos
-ubicados a su alrededor, esto con el objetivo de minimizar el riesgo de choques. El
-sistema debe mostrar una vista grafica del plantel recomendado, por ejemplo:
-Para alquilar un vehículo primeramente se genera una solicitud de alquiler. Cada
-solicitud debe tener un código o ID único, la identificación del cliente, del
-colaborador responsable, de la sucursal, la placa del vehículo, la cantidad de días
-B00 B01 B02 B03 B04 B05
-B10 B11 B12 B13 B14 B15
-B20 B21 B22 B23 B24 B25
-B30 B31 B32 B33 B34 B35
-B40 B41 B42 B43 B44 B45
+	Cuando un vehículo es alquilado deja disponible su espacio en el estacionamiento y cuando es devuelto, el sistema debe recomendar un lugar específico para estacionar
+el vehículo que ingresa. El sistema debe recomendar primeramente el plantel de la sucursal con más espacios de estacionamiento disponibles. Una vez elegido el plantel,
+el sistema debe recomendar los espacios de estacionamiento con menos vehículos ubicados a su alrededor, esto con el objetivo de minimizar el riesgo de choques. El
+sistema debe mostrar una vista grafica del plantel recomendado, por ejemplo: Para alquilar un vehículo primeramente se genera una solicitud de alquiler. Cada
+solicitud debe tener un código o ID único, la identificación del cliente, del colaborador responsable, de la sucursal, la placa del vehículo, la cantidad de días
 Espacios de estacionamiento
-recomendados:
-B40
-B30
-B05
+recomendados: Donde al rededor hay la menor cantidad de vehiculos
 Nota: El sistema recomienda
-y el usuario decide el espacio de
-estacionamiento por asignar.
-
-	*/
-
+y el usuario decide el espacio de estacionamiento por asignar. */
 {
+private:
+	Vehiculo*** espacios;
+	int nF, nC;
+public:
+	EspacioEstacionamiento(int capMax);
+	~EspacioEstacionamiento();
+	string espacioRecomendado();
+	void eliminarVehiAlquilado();
+	bool esPrimo(int num);
 };
 
